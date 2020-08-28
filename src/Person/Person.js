641f69d1e72861ecd737840cb5d567c38ee0f107
KeiferE <keifer.edelmayer@gmail.com>
2020-08-28 16:25:16 -0700

    example with using css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,36 +2,18 @@ import React from 'react';
 import Radium from 'radium';
 import styled from 'styled-components';
 
-// import './Persons.css';
+import classes from'./Persons.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-
-    const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-            width: 450px;
-
-    }
-    `;
+  
 
     return ( 
     // <div className="Persons" style={style}>
-    <StyledDiv>
+    <div className={classes.Persons}>
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
-    </StyledDiv>
+    </div>
     )
 }
 
