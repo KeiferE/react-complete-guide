895f38919477fb7075b38dc0e78ebbb98b9c67a9
KeiferE <keifer.edelmayer@gmail.com>
2020-09-01 23:38:25 -0700

    wiped app jsx file and created a layout comp to use for burger builder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,124 +1,14 @@
 import React, { Component } from 'react';
-import classes from './App.css';
-import Person, { StyleRoot } from '../components/Persons/Person/Person';
-import Radium from 'radium';
-import styled from 'styled-components';
-import Persons from '../components/Persons/Persons'
-import Cockpit from '../components/Cockpit/Cockpit'
-
+import Layout from '../components/Layout/Layout'
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
-    
-  }
-
-  state = {
-    persons: [
-      {id: 'fdgjfdg2', name: 'Max', age: 28 },
-      {id: 'dgggg1', name: 'Manu', age: 29 },
-      {id: 'zdvcxv2', name: 'Steph', age: 32}
-    ],
-    showPersons: false,
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
-  }
-
-  componentDidMount() {
-    console.log('[App.js] componentDidMount');
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate');
-    return true;
-  }
-
-  componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate');
-  }
-
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice(); old method, es6 way below
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
-
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    const person = { 
-      ...this.state.persons[personIndex]
-    }; 
-
-    // Old method const person = Object.assign({}, this.state.persons[personIndex]);
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({persons: persons
-    })
-  }
-
-  togglerPersonHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
-
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
-    
-    console.log('[App.js] render');
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <Persons 
-        persons={this.state.persons}
-        clicked={this.deletePersonHandler}
-        changed={this.nameChangedHandler}/> 
-      );
-
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
-      // btnClass.push(classes.Red);
-    }
-
-    
     return (
-      <div className={classes.App}>
-        <Cockpit 
-        title={this.props.appTitle}
-        showPersons={this.state.showPersons}
-        persons={this.state.persons}
-        clicked={this.togglerPersonHandler} />
-        {persons}
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!! '));
   }
 }
-
 export default App;
